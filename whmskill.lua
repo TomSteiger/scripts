051faf90c060a8e2092134d052d31f44e4eb7b06
Tom Steiger <tsteiger@biotrust.com>
2015-09-03 19:57:09 -0600

    Fixes

--- whmskill.lua
+++ whmskill.lua
@@ -57,7 +57,8 @@ windower.add_to_chat(121,'Healing Skill Cap for current level: '..tblHealingCap[
 windower.add_to_chat(121,'Enhancing Skill Cap for current level: '..tblEnhancingCap[iPlyrLevel])
 
 --Get Cure spell IDs
-windower.add_to_chat(121,res.spells:with('name', 'Cure'))
+iSpellID = tonumber(res.spells:with('name', 'Cure')
+windower.add_to_chat(121,iSpellID)
 --tblCures[1] = res.spells:with('name', 'Cure')
 
 --sTemp = tblCures[1]
